f606adc21123c68f21aa959e78f12b00d9492e5f
Jerry Abramson <21062491+jerryabramson@users.noreply.github.com>
2025-01-12 00:59:48 -0500

    simplify

--- newTestBandWidth.sh
+++ newTestBandWidth.sh
@@ -1,9 +1,2 @@
 #!/usr/bin/env bash
-CWD=$(pwd)
-EXEDIR=$(dirname $0)
-if [[ "$EXEDIR" == "." ]]; then
-   DIR=$CWD
-else
-   DIR=$EXEDIR
-fi
-java -jar /usr/local/lib/BandWidthTester.jar $@
+java -jar /usr/local/lib/BandWidthTester.jar "$@"
