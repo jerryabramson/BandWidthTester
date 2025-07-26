4bbb4d3da529cf1289b620c80b567183d480c753
Jerry Abramson <21062491+jerryabramson@users.noreply.github.com>
2025-07-26 16:05:08 -0400

    install.sh fix

--- install.sh
+++ install.sh
@@ -8,11 +8,11 @@ gradle clean shadowJar; RC=$?
 if [[ $RC -eq 0 ]]; then
     echo
     echo
-    printf "[\033[32;7mSTAGE 2\033[0m] \033[33m----------------------- <\033[0m \033[32;7mInstalling\033[0m: \033[35m/usr/local/bin/${ARTIFACT}.jar \033[0m  \033[33m>-----------------------\033[0m\n"
+    printf "[\033[32;7mSTAGE 2\033[0m] \033[33m----------------------- <\033[0m \033[32;7mInstalling\033[0m: \033[35m/usr/local/lib/${ARTIFACT}.jar \033[0m  \033[33m>-----------------------\033[0m\n"
     echo "sudo cp -p build/${ARTIFACT}-${VERSION}-${WITHDEP}.jar /usr/local/lib/${ARTIFACT}.jar"
     sudo cp -p build/libs/${ARTIFACT}-${VERSION}-${WITHDEP}.jar /usr/local/lib/${ARTIFACT}.jar; RC=$?
     if [[ $RC -eq 0 ]]; then
-    	echo "sudo cp -p newTestBandWidth.sh /usr/local/bin/"
+    	echo "sudo cp -p newTestBandWidth.sh /usr/local/lib/"
     	sudo cp -p newTestBandWidth.sh /usr/local/bin/; RC=$?
     fi	
     echo
